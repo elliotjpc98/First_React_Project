e0a4c1c9507f6a94a82d36d268f0b6d0994931be
Elliot P. <elliotjpc98@gmail.com>
2026-04-28 23:27:11 -0400

    Created Tab Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import { CORE_CONCEPTS } from './data';
+import { CORE_CONCEPTS, EXAMPLES } from './data';
 import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcepts/CoreConcept';
 import TabButton from './components/TabButton/TabButton';
-
+import { useState } from 'react';
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+  console.log("App is rendered");
+  let tabContent = <p>Please Select a Topic</p>;
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>
+            {EXAMPLES[selectedTopic].code}
+          </code>
+        </pre>
+
+      </div>
+    )
+  }
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
+  }
   return (
     <div>
       <Header />
@@ -37,13 +58,14 @@ function App() {
           </ul>
         </section>
         <section id="examples">
-            <h2>Examples</h2>
-            <menu>
-              <TabButton>Components</TabButton>
-              <TabButton>JSX</TabButton>
-              <TabButton>Props</TabButton>
-              <TabButton>State</TabButton>
-            </menu>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+          </menu>
+          {tabContent}
         </section>
       </main>
     </div>
--- src/components/TabButton/TabButton.jsx
+++ src/components/TabButton/TabButton.jsx
@@ -1,13 +1,14 @@
 import './TabButton.css';
 
-export default function TabButton({children}){
-    function handleClick(){
-        console.log('Hello World');
-    }
+export default function TabButton({children, onSelect}){
+    console.log("Tab Button is Rendered");
+    // function handleClick(){
+    //     console.log('Hello World');
+    // }
     //closure
     return(
         <li>
-            <button onClick={handleClick}>{children}</button>
+            <button onClick={onSelect}>{children}</button>
         </li>
     )
 }
